73e8ccaeb861b7cbb78f3f5b058635f1667a7e9a
TheSnowfield <thesnowfield@moezsoft.com>
2021-02-07 08:48:41 +0800

    Group message chain

--- Konata.Core/Event/EventModel/GroupMessageEvent.cs
+++ Konata.Core/Event/EventModel/GroupMessageEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Konata.Core.Event.EventModel
 {
@@ -12,6 +13,113 @@ namespace Konata.Core.Event.EventModel
 
         public string MemberCard { get; set; }
 
-        public string MessageContent { get; set; }
+        public List<GroupMessageChain> Message { get; set; }
+
+        public uint MessageTime { get; set; }
+
+        public uint SliceTotal { get; set; }
+
+        public uint SliceIndex { get; set; }
+
+        public uint SliceFlags { get; set; }
+
+        public override string ToString()
+        {
+            if (Message == null)
+                return "";
+
+            var content = "";
+            foreach (var element in Message)
+            {
+                content += element.ToString();
+            }
+
+            return content;
+        }
+    }
+
+    public class GroupMessageChain
+    {
+        public enum ChainType
+        {
+            At,         // At
+            Text,       // Ttext
+            Image,      // Image
+            Record,     // Record
+            Emoji,      // QQ Emoji
+            QFace,      // QQ Face
+        }
+
+        public ChainType Type { get; set; }
+    }
+
+    public class ChainAt : GroupMessageChain
+    {
+        public uint AtUin { get; set; }
+
+        public ChainAt()
+            => Type = ChainType.At;
+
+        public override string ToString()
+            => $"[KQ:at,qq={(AtUin == 0 ? "all" : AtUin.ToString())}]";
+    }
+
+    public class ChainText : GroupMessageChain
+    {
+        public string Content { get; set; }
+
+        public ChainText()
+            => Type = ChainType.Text;
+
+        public override string ToString()
+            => Content;
+    }
+
+    public class ChainEmoji : GroupMessageChain
+    {
+        public uint CodePoint;
+
+        public ChainEmoji()
+            => Type = ChainType.Emoji;
+
+        public override string ToString()
+            => $"[KQ:emoji,id={CodePoint}]";
+    }
+
+    public class ChainQFace : GroupMessageChain
+    {
+        public uint FaceId;
+
+        public ChainQFace()
+            => Type = ChainType.QFace;
+
+        public override string ToString()
+            => $"[KQ:face,id={FaceId}]";
+    }
+
+    public class ChainImage : GroupMessageChain
+    {
+        public string ImageUrl { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FileHash { get; set; }
+
+        public ChainImage()
+            => Type = ChainType.Image;
+
+        public override string ToString()
+            => $"[KQ:image,file={FileName}]";
+    }
+
+    public class ChainRecord : GroupMessageChain
+    {
+        public string FileName { get; set; }
+
+        public ChainRecord()
+            => Type = ChainType.Record;
+
+        public override string ToString()
+            => $"[KQ:record,file={FileName}]";
     }
 }
